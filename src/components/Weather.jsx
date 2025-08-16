d21252555fe4b196cc5a834273510d7bb1108d1e
Yudistiraa33 <inyomanyudistira33@gmail.com>
2025-08-16 19:41:11 +0800

    update api

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -13,17 +13,37 @@ const Weather = () => {
 
   const [weatherData, setWeatherData] = useState(false);
 
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+    "02n": cloud_icon,
+    "03d": cloud_icon,
+    "03n": cloud_icon,
+    "04d": drizzle_icon,
+    "04n": drizzle_icon,
+    "09d": rain_icon,
+    "09n": rain_icon,
+    "10d": rain_icon,
+    "10n": rain_icon,
+    "13d": snow_icon,
+    "13n": snow_icon,
+  }
+
 const search = async (city) => {
    try {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
 
       const response = await fetch(url);
       const data = await response.json();
       console.log(data);
+      const icon = allIcons[data.weather[0].icon] || clear_icon
       setWeatherData({
         humidity: data.main.humidity,
         windSpeed: data.wind.speed,
-        temperature: data.main.temp
+        temperature: Math.floor(data.main.temp),
+        location: data.name
+        icon:
       })
 
    } catch (error) {
